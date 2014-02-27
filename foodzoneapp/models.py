ef1d7d8e73e617780e40b8c36b1cde39b5e6d115
Iván Aveiga <ijaveiga@gmail.com>
2014-02-27 09:34:05 -0500

    No sé que cambié :S

--- foodzoneapp/models.py
+++ foodzoneapp/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 
-# Create your models here.
-
+#Zona
 class Zona(models.Model):
     descripcion = models.CharField(max_length = 50)
     latitud = models.FloatField()
     longitud = models.FloatField()
     activo = models.BooleanField(default = True)
 
+
+
+#Tipo de Local Ejemplo: Bar, Restaurante, Quiosco
 class TipoLocal(models.Model):
+    tipos = ((1,'Bar'), (2,'Restaurante'), (3,'Quiosco'))
+    #id_tipo = models.SmallIntegerField(primary_key = True, choices = tipos, default = 1)
     descripcion = models.CharField(max_length = 60)
-    
+
+
+
+#Local    
 class Local(models.Model):
     nombre = models.CharField(max_length = 30)
     tipo_local = models.ForeignKey(TipoLocal)
-    direccion = models.CharField(max_length = 50, blank = True)
-    resena = models.CharField(max_length = 60, blank = True)
+    direccion = models.CharField(max_length = 50)
+    resena = models.CharField(max_length = 60)
     latitud = models.FloatField()
     longitud = models.FloatField()
     activo = models.BooleanField(default = True)
@@ -24,6 +31,9 @@ class Local(models.Model):
     fechaIngreso = models.DateTimeField("Fecha de Ingreso")
     userIngreso = models.IntegerField()
 
+
+
+#Plato
 class Plato(models.Model):
     nombre = models.CharField(max_length = 30)
     descripcion = models.CharField(max_length = 50)
@@ -32,3 +42,6 @@ class Plato(models.Model):
     fechaIngreso = models.DateTimeField("Fecha de Ingreso")
     precio = models.FloatField()
     id_local = models.ForeignKey(Local)
+    
+    #Debe ser nullable porque hay la posibilidad de no subir foto.
+    #image = models.ImageField(upload_to='image/platos/', null=True)
